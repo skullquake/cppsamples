dbaa28933559ab7c16b00888cc08f86570bf7810
skullquake <ockert8080@gmail.com>
2019-12-09 19:34:03 +0200

    lists

--- src/list/src/a.cpp
+++ src/list/src/a.cpp
@@ -1,35 +1,33 @@
-#include<vector>
+#include<list>
 #include<iostream>
 int main(int argc,char** argv){
-	std::vector<int> a;
+	std::list<int> a;
 	for(int b=0;b<4;b++)
 		a.push_back(b);
-	for(int b=0;b<a.size();b++)
-		std::cout<<a[b]<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
 	for(auto b:a)
 		std::cout<<b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
-	for(std::vector<int>::iterator b=a.begin();b!=a.end();++b)
+	for(std::list<int>::iterator b=a.begin();b!=a.end();++b)
 		std::cout<<*b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
 	for(auto b=a.begin();b!=a.end();++b)
 		std::cout<<*b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
-	for(std::vector<int>::const_iterator b=a.cbegin();b!=a.cend();++b)
+	for(std::list<int>::const_iterator b=a.cbegin();b!=a.cend();++b)
 		std::cout<<*b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
 	for(auto b=a.cbegin();b!=a.cend();++b)
 		std::cout<<*b<<std::endl;
 
 	std::cout<<"----------------------------------------"<<std::endl;
-	for(std::vector<int>::reverse_iterator b=a.rbegin();b!=a.rend();++b)
+	for(std::list<int>::reverse_iterator b=a.rbegin();b!=a.rend();++b)
 		std::cout<<*b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
 	for(auto b=a.crbegin();b!=a.crend();++b)
 		std::cout<<*b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
-	for(std::vector<int>::const_reverse_iterator b=a.crbegin();b!=a.crend();++b)
+	for(std::list<int>::const_reverse_iterator b=a.crbegin();b!=a.crend();++b)
 		std::cout<<*b<<std::endl;
 	std::cout<<"----------------------------------------"<<std::endl;
 	for(auto b=a.rbegin();b!=a.rend();++b)
