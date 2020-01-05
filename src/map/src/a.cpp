28e89be941bf965ce32e2ea1dcf0e55123067457
skullquake <ockert8080@gmail.com>
2020-01-05 23:41:46 +0200

    .

--- src/map/src/a.cpp
+++ src/map/src/a.cpp
@@ -25,7 +25,6 @@ int main(int argc,char** argv){
 	{
 		//map of string and lambda
 		std::map<std::string,std::function<void(void)>> a;
-		auto b=[](){};
 		for(int i=0;i<8;i++){
 			a.insert(
 				std::pair<
@@ -46,6 +45,84 @@ int main(int argc,char** argv){
 			;
 			b.second();//call lambda
 			std::cout<<"----------------------------------------"<<std::endl;
+		}
+	}
+	{
+		//find
+		std::map<std::string,std::string> a;
+		for(int i=0;i<8;i++){
+			a.insert(
+				std::pair<std::string,std::string>(
+					"k"+std::to_string(i),
+					"v"+std::to_string(i)
+				)
+			);
+		}
+		if(a.find("k1")!=a.end()){
+			std::cout<<"a[\"k1\"]:"<<a["k1"]<<std::endl;
+		}else{
+			std::cerr<<"a[\"k1\"] not found"<<std::endl;
+		}
+	}
+	{
+		//simple menu
+		bool done=false;
+		std::map<std::string,std::function<void(void)>> a;
+		a.insert(
+			std::pair<
+				std::string,
+				std::function<void(void)>
+			>(
+				"1",
+				[=](){
+					std::cout<<"Option 1 chosen"<<std::endl;
+				}
+			)
+		);
+		a.insert(
+			std::pair<
+				std::string,
+				std::function<void(void)>
+			>(
+				"2",
+				[=](){
+					std::cout<<"Option 2 chosen"<<std::endl;
+				}
+			)
+		);
+		a.insert(
+			std::pair<
+				std::string,
+				std::function<void(void)>
+			>(
+				"q",
+				[&](){
+					std::cout<<"Quitting"<<std::endl;
+					done=true;
+				}
+			)
+		);
+		std::cout<<"----------------------------------------"<<std::endl;
+		std::cout<<"Menu"<<std::endl;
+		std::cout<<"----------------------------------------"<<std::endl;
+		while(!done){
+			int idx=0;
+			for(auto b:a){
+				std::cout
+					<<idx++
+					<<". "
+					<<b.first
+					<<std::endl
+				;
+			}
+			std::string selection;
+			std::cin>>selection;
+			if(a.find(selection)!=a.end()){
+				a[selection]();
+			}else{
+				std::cerr<<"Invalid options"<<std::endl;
+			}
+
 		}
 	}
 	return 0;
