ff19303b1200920ae0e7d38e1c544974bd8bf511
skullquake <ockert8080@gmail.com>
2020-07-31 03:48:22 -0700

    .

---
+++ src/std_find_end/src/a.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<functional>
+#include<vector>
+#include<cstdlib>
+int main(void){
+	std::vector<int>a{0,1,2,3,0,1,2,3};
+	std::vector<int>b{0,1,2,3};
+	{
+		auto c=std::find_end(a.begin(),a.end(),b.begin(),b.end());
+		if(c!=a.end()){
+			std::cout<<"found:"<<(*c)<<std::endl;
+		}else{
+			std::cout<<"not found"<<std::endl;
+		}
+	}
+	{
+		auto c=std::find_end(a.begin(),a.end(),b.begin(),b.end(),[](const auto&a,const auto&b){return a==b;});
+		if(c!=a.end()){
+			std::cout<<"found:"<<(*c)<<std::endl;
+		}else{
+			std::cout<<"not found"<<std::endl;
+		}
+	}
+	{
+		auto c=std::find_end(a.begin(),a.end(),b.begin(),b.end(),std::bind(std::equal_to<int>(),std::placeholders::_1,std::placeholders::_2));
+		if(c!=a.end()){
+			std::cout<<"found:"<<(*c)<<std::endl;
+		}else{
+			std::cout<<"not found"<<std::endl;
+		}
+	}
+	return EXIT_SUCCESS;
+}
