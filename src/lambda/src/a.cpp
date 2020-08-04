de9cca5c33ad5029814d74fc0bcc7b20cc087e95
skullquake <ockert8080@gmail.com>
2020-08-04 02:02:05 -0700

    .

--- src/lambda/src/a.cpp
+++ src/lambda/src/a.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+#include<vector>
+#include<algorithm>
+#include<thread>
+#include<unistd.h>
 int main(int argc,char** argv){
-	//basic examples
-	{
+	{//basic examples
 		auto a=[](){
 			std::cout<<"test"<<std::endl;
 		};
@@ -26,8 +29,8 @@ int main(int argc,char** argv){
 	{
 		std::cout<<[]()->int{return 0;}()<<std::endl;;
 	}
-	{
-		srand(time(0));
+	srand(time(0));
+	{//in loops
 		for(;[]()->bool{
 			int r=rand();
 			std::cout<<r<<std::endl;
@@ -44,7 +47,7 @@ int main(int argc,char** argv){
 			return r%2==0;
 		}());
 	}
-	{
+	{//pad out for loop
 		for(int i=[&]()->int{
 			std::cout<<"init: "<<i<<std::endl;
 			return 0;
@@ -55,6 +58,26 @@ int main(int argc,char** argv){
 			++i;
 			std::cout<<"inc:  "<<i<<std::endl;
 		}())[](){}();
+	}
+	{//vector
+		std::vector<void(*)(void)> vf{
+			[](){std::cout<<"f0"<<std::endl;},
+			[](){std::cout<<"f1"<<std::endl;},
+			[](){std::cout<<"f2"<<std::endl;},
+			[](){std::cout<<"f3"<<std::endl;}
+		};
+		std::for_each(vf.begin(),vf.end(),[](auto&f){f();});
+	}
+	{//vector
+		std::vector<void(*)(void)> vf{
+			[](){usleep(((rand()%1000)*1000));std::cout<<"f0"<<std::endl;},
+			[](){usleep(((rand()%1000)*1000));std::cout<<"f1"<<std::endl;},
+			[](){usleep(((rand()%1000)*1000));std::cout<<"f2"<<std::endl;},
+			[](){usleep(((rand()%1000)*1000));std::cout<<"f3"<<std::endl;}
+		};
+		std::vector<std::thread>vt;
+		std::for_each(vf.begin(),vf.end(),[&vt](auto f){vt.push_back(std::thread(f));});
+		std::for_each(vt.begin(),vt.end(),[](auto&t){t.join();});
 	}
 	return 0;
 }
